527910ef3d81bbd84014aa9119fbe325231aea03
CarlosOrtiz9901 <carlos.ortiz@udla.edu.co>
2021-03-03 19:10:35 -0500

    feat(biodiversity): validate url null

--- src/biodiversity/biodiversity.service.ts
+++ src/biodiversity/biodiversity.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { taxonomy } from '../entities/taxonomy';
-import { Repository } from 'typeorm';
+import { NoVersionOrUpdateDateColumnError, Repository } from 'typeorm';
 import { KingdomType, KingdomTypeUrl } from './kingdom.enum'
 const puppeteer = require('puppeteer');
 @Injectable()
@@ -25,40 +25,73 @@ export class BiodiversityService {
         if (imagen.includes('http')) {
           if (!(imagen.includes('https://static.inaturalist.org/photos/') || imagen.includes('https://inaturalist-open-data.s3.amazonaws.com/photos/')))
             if (!imagen.includes('http://creativesafari.com'))
-              if (!imagen.includes('https://upload.wikimedia.org'))
-                if (imagen.includes('https://data.biodiversitydata.nl/xeno-canto/observation/'))
-                  await this.taxonomyRepository.update(id, { imagen })
-                else if (imagen == KingdomTypeUrl.Animalia)
-                  await this.taxonomyRepository.update(id, { imagen })
-                else if (imagen == KingdomTypeUrl.Chromista)
-                  await this.taxonomyRepository.update(id, { imagen })
-                else if (imagen == KingdomTypeUrl.Fungi)
-                  await this.taxonomyRepository.update(id, { imagen })
-                else if (imagen == KingdomTypeUrl.Plantae)
-                  await this.taxonomyRepository.update(id, { imagen })
-                else if (imagen == KingdomTypeUrl.Protozoa)
-                  await this.taxonomyRepository.update(id, { imagen })
-                else if (imagen == KingdomTypeUrl.default)
-                  await this.taxonomyRepository.update(id, { imagen })
-                else {
-                  browser = await puppeteer.launch({ headless: true, args: ["--disable-notifications"] })
-                  page = await browser.newPage();
+              if (!imagen.includes('https://static.inaturalist.org/sounds/'))
+                if (!imagen.includes('https://upload.wikimedia.org'))
+                  if (imagen.includes('https://data.biodiversitydata.nl/xeno-canto/observation/'))
+                    await this.taxonomyRepository.update(id, { imagen })
+                  else if (imagen == KingdomTypeUrl.Animalia)
+                    await this.taxonomyRepository.update(id, { imagen })
+                  else if (imagen == KingdomTypeUrl.Chromista)
+                    await this.taxonomyRepository.update(id, { imagen })
+                  else if (imagen == KingdomTypeUrl.Fungi)
+                    await this.taxonomyRepository.update(id, { imagen })
+                  else if (imagen == KingdomTypeUrl.Plantae)
+                    await this.taxonomyRepository.update(id, { imagen })
+                  else if (imagen == KingdomTypeUrl.Protozoa)
+                    await this.taxonomyRepository.update(id, { imagen })
+                  else if (imagen == KingdomTypeUrl.default)
+                    await this.taxonomyRepository.update(id, { imagen })
+                  else {
+                    try {
+                      browser = await puppeteer.launch({ headless: true, args: ["--disable-notifications"] })
+                      page = await browser.newPage();
 
-                  await page.goto(imagen, { waitUntil: 'networkidle2' });
-                  await page.setDefaultNavigationTimeout(30000);
+                      await page.goto(imagen, { waitUntil: 'networkidle2' });
+                      await page.setDefaultNavigationTimeout(60000);
 
-                  await page.waitForTimeout(1000)
-                  let urlTransformada = await page.$eval(("div[class='image-gallery-image'] > img[src]"), node => node.src);
-                  if (!urlTransformada)
-                    urlTransformada = KingdomTypeUrl.Animalia
+                      await page.waitForTimeout(1000)
+                      let urlTransformada = await page.$eval(("div[class='image-gallery-image'] > img[src]"), node => node.src)
+                      // let urlVideo=  await page.$eval(("div.sound > audio > source[src]"), node => node.src)
 
-                  console.log(`id=${id}:`, await urlTransformada)
-                  await this.taxonomyRepository.update(id, { imagen: urlTransformada || imagen })
-                  await page.waitForTimeout(1000)
+                      switch (value.reino) {
+                        case KingdomType.Animalia:
+                          url = KingdomTypeUrl.Animalia
+                          break
+                        case KingdomType.Chromista:
+                          url = KingdomTypeUrl.Chromista
+                          break
+                        case KingdomType.Fungi:
+                          url = KingdomTypeUrl.Fungi
+                          break
+                        case KingdomType.Plantae:
+                          url = KingdomTypeUrl.Plantae
+                          break
+                        case KingdomType.Protozoa:
+                          url = KingdomTypeUrl.Protozoa
+                          break
+                        default:
+                          url = KingdomTypeUrl.default
+                          break
+                      }
 
-                  await page.close();
-                  await browser.close();
-                }
+                      await urlTransformada ? urlTransformada : url
+
+                      console.log(`id=${id}:`, await urlTransformada)
+                      await this.taxonomyRepository.update(id, { imagen: urlTransformada || imagen })
+
+                      await page.waitForTimeout(1000)
+
+                      await page.close();
+                      await browser.close();
+                    } catch (error) {
+                      await page.close();
+                      await browser.close();
+                      console.log('TRY_puppeteer')
+                      console.log(error)
+                      return { error }
+                    }
+
+                  }
         } else {
           switch (value.reino) {
             case KingdomType.Animalia:
@@ -86,9 +119,7 @@ export class BiodiversityService {
       }
       return { success: 'OK' }
     } catch (error) {
-      await page.close();
-      await browser.close();
-      console.log('error', error)
+      console.log('err', error)
       return { error }
     }
   }
